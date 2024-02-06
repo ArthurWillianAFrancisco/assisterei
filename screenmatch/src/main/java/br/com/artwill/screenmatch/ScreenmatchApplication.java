7822170261debcbc23186616d5f0071a5b43ffef
Arthur <Arthurwilliaf@gmail.com>
2024-02-06 08:54:14 -0400

    .

--- screenmatch/src/main/java/br/com/artwill/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/artwill/screenmatch/ScreenmatchApplication.java
@@ -18,12 +18,12 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&&apikey=47a9f996");
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=onimai&&apikey=47a9f996");
 		System.out.println(json);
 		ConverteDados conversor = new ConverteDados();
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 		System.out.println(dados);
-		json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=47a9f996");
+		json = consumoApi.obterDados("https://omdbapi.com/?t=onimai&season=1&episode=2&apikey=47a9f996");
 		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
 		System.out.println(dadosEpisodio);
 	}
